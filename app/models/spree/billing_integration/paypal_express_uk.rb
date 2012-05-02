f7b2775b7e7e8e8274093eeac17cac63e821f4ba
Ryan Bigg <radarlistener@gmail.com>
2012-05-02 10:50:36 -0500

    Add missing paypalexpressuk partial and clean up the related class some
    Fixes #49

--- app/models/spree/billing_integration/paypal_express_uk.rb
+++ app/models/spree/billing_integration/paypal_express_uk.rb
@@ -4,11 +4,13 @@ class Spree::BillingIntegration::PaypalExpressUk < Spree::BillingIntegration
   preference :signature, :string
   preference :review, :boolean, :default => false
   preference :no_shipping, :boolean, :default => false
+  preference :currency, :string, :default => 'GBP'
+  preference :allow_guest_checkout, :boolean, :default => false
 
-  attr_accessible :preferred_login, :preferred_password, :preferred_signature, :preferred_review, :preferred_no_shipping, :preferred_server, :preferred_test_mode
+  attr_accessible :preferred_login, :preferred_password, :preferred_signature, :preferred_review, :preferred_currency, :preferred_no_shipping, :preferred_server, :preferred_test_mode
 
   def provider_class
-    ActiveMerchant::Billing::PaypalExpressUkGateway
+    ActiveMerchant::Billing::PaypalExpressGateway
   end
 
 end
