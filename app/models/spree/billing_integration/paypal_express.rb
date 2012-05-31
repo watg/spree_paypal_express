26da5770907c396aa8e9d16d5e7685311e3b7240
Bounmy Stephane <stephanebounmy@gmail.com>
2012-05-31 08:54:05 +0200

    using a base class for paypal express

--- app/models/spree/billing_integration/paypal_express.rb
+++ app/models/spree/billing_integration/paypal_express.rb
@@ -1,19 +1,3 @@
-class Spree::BillingIntegration::PaypalExpress < Spree::BillingIntegration
-  preference :login, :string
-  preference :password, :password
-  preference :signature, :string
-  preference :review, :boolean, :default => false
-  preference :no_shipping, :boolean, :default => false
+class Spree::BillingIntegration::PaypalExpress < Spree::BillingIntegration::PaypalExpressBase
   preference :currency, :string, :default => 'USD'
-  preference :allow_guest_checkout, :boolean, :default => false
-
-  attr_accessible :preferred_login, :preferred_password, :preferred_signature, :preferred_review, :preferred_no_shipping, :preferred_currency, :preferred_allow_guest_checkout, :preferred_server, :preferred_test_mode
-
-  def provider_class
-    ActiveMerchant::Billing::PaypalExpressGateway
-  end
-  
-  def payment_profiles_supported?
-    !!preferred_review
-  end
 end
--- app/models/spree/billing_integration/paypal_express_uk.rb
+++ app/models/spree/billing_integration/paypal_express_uk.rb
@@ -1,19 +1,3 @@
-class Spree::BillingIntegration::PaypalExpressUk < Spree::BillingIntegration
-  preference :login, :string
-  preference :password, :password
-  preference :signature, :string
-  preference :review, :boolean, :default => false
-  preference :no_shipping, :boolean, :default => false
+class Spree::BillingIntegration::PaypalExpressUk < Spree::BillingIntegration::PaypalExpressBase
   preference :currency, :string, :default => 'GBP'
-  preference :allow_guest_checkout, :boolean, :default => false
-
-  attr_accessible :preferred_login, :preferred_password, :preferred_signature, :preferred_review, :preferred_currency, :preferred_no_shipping, :preferred_server, :preferred_test_mode
-
-  def provider_class
-    ActiveMerchant::Billing::PaypalExpressGateway
-  end
-
-  def payment_profiles_supported?
-    !!preferred_review
-  end
 end
