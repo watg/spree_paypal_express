5a048d4449886c95835fe9afdbd345d2836d206d
Ryan Bigg <radarlistener@gmail.com>
2012-11-07 09:26:00 +1100

    Strip all potential blank characters from Spree::Config[:site_url] inside asset_url method

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -177,7 +177,7 @@ module Spree
     private
 
     def asset_url(_path)
-      URI::HTTP.build(:path => ActionController::Base.helpers.asset_path(_path), :host => Spree::Config[:site_url]).to_s
+      URI::HTTP.build(:path => ActionController::Base.helpers.asset_path(_path), :host => Spree::Config[:site_url].strip).to_s
     end
 
     def record_log(payment, response)
