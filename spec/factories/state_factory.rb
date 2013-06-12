b1947a6b608de5b3ab186e105bcaaad4b008ee21
Michał Szajbe <michal.szajbe@gmail.com>
2013-06-12 15:58:19 +0200

    Fixed FactoryGirl deprecation warnings

--- spec/factories/state_factory.rb
+++ spec/factories/state_factory.rb
@@ -1,5 +1,7 @@
-Factory.define :md_state do |f|
-  f.name 'Maryland'
-  f.abbr 'MD'
-  f.country { |country| country.association(:country) }
+FactoryGirl.define do
+  factory :md_state do |f|
+    f.name 'Maryland'
+    f.abbr 'MD'
+    f.country { |country| country.association(:country) }
+  end
 end
