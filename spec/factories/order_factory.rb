f5c42385d58772d3840cd48174003b63b17b39c7
Michał Szajbe <michal.szajbe@gmail.com>
2013-06-13 10:17:18 +0200

    Use factories from Spree core

--- spec/factories/order_factory.rb
+++ spec/factories/order_factory.rb
@@ -1,10 +1,9 @@
 FactoryGirl.define do
   factory :ppx_order do |record|
-    # associations:
-    record.association(:user, :factory => :user)
-    record.association(:bill_address, :factory => :address)
+    user
+    bill_address
+    ship_address
     record.association(:shipping_method, :factory => :shipping_method)
-    record.ship_address { |ship_address| Factory(:ppx_address, :city => "Chevy Chase", :zipcode => "20815") }
   end
 
   factory :ppx_order_with_totals, :parent => :order do |f|
